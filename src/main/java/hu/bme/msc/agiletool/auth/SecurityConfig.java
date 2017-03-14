e0a1dc18a7fc094ecd103b3819538448e99a8c1f
Richard Balog <balog.richard.mail@gmail.com>
2017-03-14 22:31:21 +0100

    Env created

--- src/main/java/hu/bme/msc/agiletool/auth/SecurityConfig.java
+++ src/main/java/hu/bme/msc/agiletool/auth/SecurityConfig.java
@@ -1,16 +1,12 @@
 package hu.bme.msc.agiletool.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
@@ -18,6 +14,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MongoDBAuthenticationProvider authenticationProvider;
+
+    @Value("${app.successurl}")
+    private String successUrl;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -33,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/")
-                .defaultSuccessUrl("/app", true)
+                .defaultSuccessUrl(successUrl, true)
                 .failureUrl("/")
                 .permitAll()
                 .and()
