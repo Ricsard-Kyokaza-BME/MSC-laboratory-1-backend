82f887c91f209dd90bb1500b65593a7beea0bba8
Kazinczi Márton <Marton.Kazinczi@ericsson.com>
2017-03-16 11:01:53 +0100

    Little fix befora mass up.

--- src/test/java/hu/bme/msc/agiletool/test/Base.java
+++ src/test/java/hu/bme/msc/agiletool/test/Base.java
@@ -1,83 +1,74 @@
 package hu.bme.msc.agiletool.test;
 
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-
-import com.mongodb.util.JSON;
-import hu.bme.msc.agiletool.auth.MongoDBAuthenticationProvider;
-import hu.bme.msc.agiletool.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
-
 
 import java.util.ArrayList;
 import java.util.List;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Base {
-
     @Autowired
-    private UserRepository repository;
-
-    static final String PAYLOAD = "{\"firstName\": \"Alice\", \"username\": \"Alice\", " + "\"password\": \"abcd\"}";
-
+    private WebApplicationContext context;
     @Autowired
-    WebApplicationContext context;
-    @Autowired
-    FilterChainProxy filterChain;
-    @Autowired
-    MongoDBAuthenticationProvider mongoDBAuthenticationProvider;
+    private FilterChainProxy filterChain;
 
-    MockMvc mvc;
+    private MockMvc mvc;
+    private MvcResult result;
 
     @Before
     public void setUp() {
-
-        this.mvc = webAppContextSetup(context).addFilters(filterChain).build();
-
+        this.mvc = webAppContextSetup(context)
+                .addFilters(filterChain)
+                .build();
         SecurityContextHolder.clearContext();
     }
 
+
     @Test
     public void allowsAccessToRootResource() throws Exception {
+        mvc.perform(get("/").
+                accept(MediaTypes.HAL_JSON)).
+                andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)).
+                andExpect(status().isOk()).
+                andDo(log());
+    }
+
+    @Test
+    public void fetchingAllDataFromUser() throws Exception {
         List<GrantedAuthority> aliceRoles = new ArrayList<>();
         aliceRoles.add(new SimpleGrantedAuthority("USER"));
 
-        mvc.perform(get("/api/user").with(user(new User("alice", "abcd", aliceRoles))).
+        result = mvc.perform(get("/api/user").
+                with(user(new User("alice", "abcd", aliceRoles))).
                 accept(MediaTypes.HAL_JSON)).
-                //andExpect(content().contentTypeCompatibleWith(MediaTypes.HAL_JSON)).
+                andExpect(content().contentTypeCompatibleWith(MediaTypes.HAL_JSON)).
                 andExpect(status().isOk()).
-                andDo(print());
-
-//        @Test
-//        public void rejectsPostAccessToCollectionResource() throws Exception {
-//            User alice = new User("Alice", null, "Alice", null, "abcd", null);
-//            mvc.perform(post("/").
-//                    content(PAYLOAD).
-//                    accept(MediaTypes.HAL_JSON)).
-//                    andExpect(status().isUnauthorized()).
-//                    andDo(print());
-//        }
+                andDo(print()).
+                andReturn();
     }
-
 }
