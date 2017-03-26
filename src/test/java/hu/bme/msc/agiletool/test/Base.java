48bf992849e437324b79147b8acea94614e4ff71
Kazinczi Márton <Marton.Kazinczi@ericsson.com>
2017-03-26 20:33:11 +0200

    Bugging prject and user addition. Is signed in fixed

--- src/test/java/hu/bme/msc/agiletool/test/Base.java
+++ src/test/java/hu/bme/msc/agiletool/test/Base.java
@@ -8,6 +8,7 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -29,6 +30,7 @@ public class Base extends Preconditioning {
     @Test
     public void allowsAccessToIsSignedIn() throws Exception {
         mvc.perform(get("/api/is-signed-in").
+                with(user(testUserAlice)).
                 accept(MediaTypes.HAL_JSON)).
                 andExpect(status().isOk()).
                 andDo(print());
