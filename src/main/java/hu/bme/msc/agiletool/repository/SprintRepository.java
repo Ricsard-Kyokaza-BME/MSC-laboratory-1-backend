30af67e3cd4cdb46228e596086fbb3c5afbe9604
Richard Balog <balog.richard.mail@gmail.com>
2017-05-01 13:49:19 +0200

    Sprint PO-USER fix

--- src/main/java/hu/bme/msc/agiletool/repository/SprintRepository.java
+++ src/main/java/hu/bme/msc/agiletool/repository/SprintRepository.java
@@ -1,6 +1,7 @@
 package hu.bme.msc.agiletool.repository;
 
 import hu.bme.msc.agiletool.model.Sprint;
+import hu.bme.msc.agiletool.model.UserStory;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -9,7 +10,23 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import java.util.List;
 
 @RepositoryRestResource(path = "sprint", collectionResourceRel = "sprint")
-@PreAuthorize("hasAnyAuthority('PO')")
+@PreAuthorize("hasAnyAuthority('PO','USER')")
 public interface SprintRepository extends MongoRepository<Sprint, String> {
     List<Sprint> findById(@Param("id") String id);
+
+    @Override
+    @PreAuthorize("hasAuthority('PO')")
+    <S extends Sprint> List<S> save(Iterable<S> entities);
+
+    @Override
+    @PreAuthorize("hasAuthority('PO')")
+    <S extends Sprint> S save(S entity);
+
+    @Override
+    @PreAuthorize("hasAuthority('PO')")
+    void delete(String s);
+
+    @Override
+    @PreAuthorize("hasAuthority('PO')")
+    void delete(Sprint entity);
 }
